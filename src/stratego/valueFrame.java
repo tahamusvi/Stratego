7e64a294ad28c4568d63a73a37f03e2d1f822278
Mors <seyedsahel1383@gmail.com>
2023-09-24 06:26:21 +0330

    connecting core to gui for setting button values, phase 2

--- src/stratego/valueFrame.java
+++ src/stratego/valueFrame.java
@@ -2,6 +2,8 @@ package stratego;
 
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import javax.xml.transform.Templates;
+
 import stratego.Frame;
 import javax.swing.*;
 import java.awt.*;
@@ -53,8 +55,8 @@ public class valueFrame extends JFrame implements ActionListener{
         String test = labels[6].getText();
         System.out.println(test);
         
-        core table = new core(10,10,"mors");//این ارگومان باید عوض شه از کاربر گرفته شه
-        piece[] pieces = table.getPiecesList();
+        
+        piece[] pieces = parentFrame.table.getPiecesList();
         for(int i=0; i<12;i++) {
         	labels[i].setText(pieces[i].getLabelText());
         	buttons[i].setText(pieces[i].getName());
@@ -68,6 +70,16 @@ public class valueFrame extends JFrame implements ActionListener{
         public void actionPerformed(ActionEvent e) {
             JButton button = (JButton) e.getSource();
             selectedpiece = button.getText();
+            piece tempPiece = parentFrame.table.getPiecesList()[0];
+            for(int k=0; k<12; k++)
+            {
+            	if(parentFrame.table.getPiecesList()[k].getName()==selectedpiece)
+            	{
+            		tempPiece = parentFrame.table.getPiecesList()[k];
+            	}
+            }
+            if(tempPiece.isRemain())
+            {
             for(int i=0;i<10;i++)
             {
             	for(int j=0;j<10;j++)
@@ -77,6 +89,7 @@ public class valueFrame extends JFrame implements ActionListener{
             			parentFrame.buttons[i][j].setText(selectedpiece);
             		}
             	}
+            }
             }
             this.dispose();
         }
