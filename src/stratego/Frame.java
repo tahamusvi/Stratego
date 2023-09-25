3101f61eab144fcf49fcca382c021f145f696a94
Mors <seyedsahel1383@gmail.com>
2023-09-25 06:10:50 +0330

    creating class playFrame

--- src/stratego/Frame.java
+++ src/stratego/Frame.java
@@ -69,7 +69,7 @@ public class Frame extends JFrame implements ActionListener{
         add(submitPanel, BorderLayout.SOUTH);
     }
 
-    //static class ButtonClickListener implements ActionListener {
+   
     @Override
 	public void actionPerformed(ActionEvent e) {
             JButton button = (JButton) e.getSource();
@@ -90,6 +90,8 @@ public class Frame extends JFrame implements ActionListener{
                  );
                  if (choice == JOptionPane.YES_OPTION) {
                      System.out.println("شما گزینه بازی را انتخاب کردید.");
+                     playFrame newplayFrame = new playFrame();
+                     this.dispose();
                  } else if (choice == JOptionPane.NO_OPTION) {
                      System.out.println("شما گزینه سیو را انتخاب کردید.");
                      table.writeTxt();
@@ -140,83 +142,4 @@ public class Frame extends JFrame implements ActionListener{
 
 
 
-
-
-
-
-//public class Frame extends JFrame {
-//    private JButton[][] buttons;
-//
-//    public Frame() {
-//        super("STRATEGO");
-//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        setLayout(new GridLayout(11, 10));
-//
-//        buttons = new JButton[10][10];
-//
-//        designFrame();
-//
-//        setSize(800, 550);
-//        setVisible(true);
-//    }
-//
-//    private void designFrame() {
-//        for (int i = 0; i < 10; i++) {
-//            for (int j = 0; j < 10; j++) {
-//                JButton button = new JButton("?");
-//
-//                if (i == 6 || i == 7 || i == 8 || i == 9) {
-//                    button.setBackground(Color.red);
-//                } else if (i == 4 || i == 5) {
-//                    button.setBackground(Color.white);
-//                    button.setText("");
-//                }
-//
-//                if ((i == 4 || i == 5) && (j == 2 || j == 3 || j == 7 || j == 8)) {
-//                    button.setEnabled(false);
-//                    button.setText("w");
-//                } else {
-//                    button.addActionListener(new ButtonClickListener());
-//                }
-//
-//                buttons[i][j] = button;
-//                add(button);
-//            }
-//        }
-//
-//        JButton submitButton = new JButton("Submit");
-//        submitButton.addActionListener(new ButtonClickListener());
-//        add(submitButton);
-//
-//        for (int i = 0; i < 4; i++) {
-//            for (int j = 0; j < 10; j++) {
-//                buttons[i][j].setText("x");
-//            }
-//        }
-//    }
-//
-//    static class ButtonClickListener implements ActionListener {
-//        public void actionPerformed(ActionEvent e) {
-//            JButton button = (JButton) e.getSource();
-//            String buttonText = button.getText();
-//
-//            if (buttonText.equals("?")) {
-//                String value = JOptionPane.showInputDialog(null, "Please enter the button value", JOptionPane.INFORMATION_MESSAGE);
-//                button.setText(value);
-//            } else if (!buttonText.equals("x")) {
-//                int selectedMode = JOptionPane.showConfirmDialog(null, "Do you want to change the value of the button?");
-//                if (selectedMode == JOptionPane.YES_OPTION) {
-//                    String value = JOptionPane.showInputDialog(null, "Please enter the button value", JOptionPane.INFORMATION_MESSAGE);
-//                    button.setText(value);
-//                } else {
-//                    System.out.println("دکمه " + buttonText + " فشرده شد.");
-//                }
-//            }
-//        }
-//    }
-//
-//    public static void main(String[] args) {
-//        new Frame();
-//    }
-//}
 
