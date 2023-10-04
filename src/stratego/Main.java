8b2f419bfc8a58cf50663ca2332bdba4fc2accbe
Mors <seyedsahel1383@gmail.com>
2023-10-04 14:07:04 +0330

    edit class Main, phase 2(complete the Frame)

--- src/stratego/Main.java
+++ src/stratego/Main.java
@@ -7,7 +7,9 @@ import java.security.PublicKey;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
@@ -18,9 +20,9 @@ import java.awt.event.ActionListener;
 
 public class Main extends JFrame {
 		
-		private JTextArea wellcomArea;
-	    private JTextArea nameArea;
-	    private JTextArea whoPickArea;
+		private JLabel wellcomArea;
+		private JLabel nameArea;
+		private JLabel whoPickArea;
 	    private JTextField nameField;
 	    private JButton submitName;
 	    private JButton user;
@@ -31,16 +33,16 @@ public class Main extends JFrame {
 	    public String username;
 
 	    public Main() {
-	        // تنظیمات فریم
-	        setTitle("Game");
-	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        setSize(400, 300);
-	        setLayout(new FlowLayout());
 
-	        // ایجاد اجزا
-	        wellcomArea = new JTextArea("Welcome to our Game!");
-	        nameArea = new JTextArea("Please enter your name (if you don't, we'll use 'user'):");
-	        whoPickArea = new JTextArea("Who will pick?");
+	    	setTitle("Stratego");
+	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        setSize(320, 270);
+	        setLayout(new FlowLayout());
+	        setResizable(false);
+
+	        wellcomArea = new JLabel("Welcome to our Game!");
+	        nameArea = new JLabel("Please enter your name (if you don't, we'll use 'user'):");
+	        whoPickArea = new JLabel("Who will pick?");
 	        nameField = new JTextField(15);
 	        submitName = new JButton("Submit");
 	        user = new JButton("User");
@@ -48,24 +50,37 @@ public class Main extends JFrame {
 	        loud = new JButton("Loud");
 	        aboutUsButton = new JButton("About Us!");
 
-	        // اضافه کردن اجزا به فریم
-	        add(wellcomArea);
-	        add(nameArea);
-	        add(nameField);
-	        add(submitName);
-	        add(user);
-	        add(custom);
-	        add(loud);
-	        add(whoPickArea);
-	        add(aboutUsButton);
+	        JPanel panel1 = new JPanel();
+            JPanel panel2 = new JPanel();
+            JPanel panel3 = new JPanel();
+            JPanel panel4 = new JPanel(new BorderLayout());
+            JPanel panel5 = new JPanel();
+            JPanel panel6 = new JPanel();
+            
+	        panel1.add(wellcomArea);
+	        panel2.add(nameArea);
+	        panel3.add(nameField);
+	        panel3.add(submitName);
+	        panel4.add(whoPickArea);
+	        panel5.add(user);
+	        panel5.add(custom);
+	        panel5.add(loud);
+	        panel6.add(aboutUsButton);
+	        
+	        add(panel1);
+            add(panel2);
+            add(panel3);
+            add(panel4);
+            add(panel5);
+            add(panel6);
 
-	        // تنظیمات دکمه‌ها
+	       
 	        submitName.addActionListener(new ActionListener() {
 	            @Override
 	            public void actionPerformed(ActionEvent e) {
 	                 username = nameField.getText();
 	                if (username.isEmpty()) {
-	                    // اگر نام وارد نشده بود، از "user" استفاده می‌کنیم
+	                  
 	                	username = "user";
 	                }
 	                System.out.println("Submitted name: " + username);
@@ -75,34 +90,34 @@ public class Main extends JFrame {
 	        user.addActionListener(new ActionListener() {
 	            @Override
 	            public void actionPerformed(ActionEvent e) {
-	                //کدی که در صورت فشرده شدن دکمه "user" اجرا می‌شود
 	            	table = new core(10,10,username,1);
 	            	new pickFrame(table);
+	            	dispose();
 	            }
 	        });
 
 	        custom.addActionListener(new ActionListener() {
 	            @Override
 	            public void actionPerformed(ActionEvent e) {
-	                // کدی که در صورت فشرده شدن دکمه "custom" اجرا می‌شود
 	            	table = new core(10,10,username,2);
 	            	playFrame newplayFrame = new playFrame(table);
+	            	dispose();
 	            }
 	        });
 
 	        loud.addActionListener(new ActionListener() {
 	            @Override
 	            public void actionPerformed(ActionEvent e) {
-	                // کدی که در صورت فشرده شدن دکمه "loud" اجرا می‌شود
 	            	table = new core(10,10,username,3);
 	            	playFrame newplayFrame = new playFrame(table);
+	            	dispose();
 	            }
 	        });
 
 	        aboutUsButton.addActionListener(new ActionListener() {
 	            @Override
 	            public void actionPerformed(ActionEvent e) {
-	                // کدی که در صورت فشرده شدن دکمه "About Us!" اجرا می‌شود
+ 
 	            }
 	        });
 	    }
